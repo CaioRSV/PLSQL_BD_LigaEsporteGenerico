0f9d036281c340f151fabef277ab1eb3ee9dd55f
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-07-29 13:30:05 -0300

    Updates

--- scripts/populate.sql
+++ scripts/populate.sql
@@ -7,29 +7,29 @@ INSERT INTO Equipe VALUES ('E#004', 'Enter the Fire', TO_DATE('2019-01-01', 'YYY
 
 -- Equipe 1
 INSERT INTO Residencia VALUES('R#001', 'Brasil', 'Pernambuco', 'Vitória de St. Antão');
-INSERT INTO Jogador VALUES ('J#001', 'J1', TO_DATE('1993-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#001', 'E#001', 0, 0);
+INSERT INTO Jogador VALUES ('J#001', 'João', TO_DATE('1993-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#001', 'E#001', 0, 0);
 
 INSERT INTO Residencia VALUES('R#002','Chile', 'Estado2', 'Cidade2');
-INSERT INTO Jogador VALUES('J#002', 'J2', TO_DATE('1988-10-12', 'YYYY-MM-DD'), 'Chileno', 'R#002', 'E#001', 0, 0);
+INSERT INTO Jogador VALUES('J#002', 'Pablo', TO_DATE('1988-10-12', 'YYYY-MM-DD'), 'Chileno', 'R#002', 'E#001', 0, 0);
 
 
 -- Equipe 2
-INSERT INTO Jogador VALUES ('J#003', 'J3', TO_DATE('1995-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#001', 'E#002', 0, 0);
+INSERT INTO Jogador VALUES ('J#003', 'Pedro', TO_DATE('1995-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#001', 'E#002', 0, 0);
 
 INSERT INTO Residencia VALUES('R#003', 'Brasil', 'Pernambuco', 'Recife');
-INSERT INTO Jogador VALUES ('J#004', 'J4', TO_DATE('1998-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#003', 'E#002', 0 ,0 );
+INSERT INTO Jogador VALUES ('J#004', 'Marcos', TO_DATE('1998-10-12', 'YYYY-MM-DD'), 'Brasileiro', 'R#003', 'E#002', 0 ,0 );
 
 -- Equipe 3
 INSERT INTO Residencia VALUES('R#004', 'Australia', 'Sidney', 'SomePlaceMate');
-INSERT INTO Jogador VALUES ('J#005', 'J5', TO_DATE('1994-10-12', 'YYYY-MM-DD'), 'Australiano', 'R#004', 'E#003', 0, 0);
+INSERT INTO Jogador VALUES ('J#005', 'Max', TO_DATE('1994-10-12', 'YYYY-MM-DD'), 'Australiano', 'R#004', 'E#003', 0, 0);
 
-INSERT INTO Jogador VALUES ('J#006', 'J6', TO_DATE('1998-10-12', 'YYYY-MM-DD'), 'Australiano', 'R#004', 'E#003', 0, 0);
+INSERT INTO Jogador VALUES ('J#006', 'Scott', TO_DATE('1998-10-12', 'YYYY-MM-DD'), 'Australiano', 'R#004', 'E#003', 0, 0);
 
 -- Equipe 4
 INSERT INTO Residencia VALUES('R#005', 'China', 'Pequim', '土豆');
-INSERT INTO Jogador VALUES ('J#007', 'J7', TO_DATE('2000-10-12', 'YYYY-MM-DD'), 'Chinês', 'R#005', 'E#004', 0, 0);
+INSERT INTO Jogador VALUES ('J#007', 'Uzi', TO_DATE('2000-10-12', 'YYYY-MM-DD'), 'Chinês', 'R#005', 'E#004', 0, 0);
 
-INSERT INTO Jogador VALUES ('J#008', 'J8', TO_DATE('1997-10-12', 'YYYY-MM-DD'), 'Coreano', 'R#005', 'E#004', 0, 0);
+INSERT INTO Jogador VALUES ('J#008', 'Lee Sang', TO_DATE('1997-10-12', 'YYYY-MM-DD'), 'Coreano', 'R#005', 'E#004', 0, 0);
 
 
 -- Criando camp sample
