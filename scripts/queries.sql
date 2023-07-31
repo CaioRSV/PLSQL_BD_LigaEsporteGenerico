adeb46c0ffc71ae7b8298cfc50faab0e2613d4f2
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-07-31 08:33:48 -0300

    Update queries.sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -9,3 +9,5 @@ SELECT * FROM Campeonato;
 SELECT * FROM Campeonatos_Participados_Equipe;
 
 SELECT * FROM Jogador_Em_Equipe_Data;
+
+SELECT * FROM Jogador INNER JOIN Jogador_Em_Equipe_Data ON Jogador.id_jogador = Jogador_Em_Equipe_Data.id_jogador;
