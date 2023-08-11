47e44b39cf05c2ba2f1bd97a02c6ab7f0fb77f82
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-08-11 16:06:32 -0300

    Update queries.sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -42,31 +42,68 @@ WHERE (Campeonatos_Participados_Equipe.id_equipe = 'E#005') AND (Campeonato.tipo
 );
 ------------------
 
-CREATE OR REPLACE FUNCTION ContarJogadorRank1Mundial(
-    p_id_jogador IN Jogador.id_jogador%TYPE
-)
-RETURN NUMBER
-IS
-    v_count NUMBER := 0;
+CREATE OR REPLACE FUNCTION ClassificacoesCampeonato (input_camp text) RETURNS TABLE AS $$
 BEGIN
-    SELECT COUNT(*) INTO v_count
-    FROM Jogador j
-    JOIN Jogador_Em_Equipe_Data jed ON j.id_jogador = jed.id_jogador
-    JOIN Equipe e ON jed.equipe_na_data = e.id_equipe
-    JOIN Campeonatos_Participados_Equipe cpe ON e.id_equipe = cpe.id_equipe
-    JOIN Campeonato c ON cpe.id_campeonato = c.id_campeonato
-    WHERE j.id_jogador = p_id_jogador
-    AND c.tipo = 'Mundial'
-    AND cpe.ranking = 1;
+    RETURN QUERY (SELECT Equipe.nome, Campeonatos_Participados_Equipe.ranking 
+    FROM Equipe INNER JOIN Campeonatos_Participados_Equipe
+    WHERE Equipe.id_equipe = input_camp AND
+    WHERE Equipe.id_equipe = Campeonatos_Participados_Equipe
+    )
 
-    RETURN v_count;
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        RETURN 0;
-    WHEN OTHERS THEN
-        -- Handle exceptions here if needed
-        RETURN -1;
-END;
-/
+END
 
-SELECT ContarJogadorRank1Mundial('J#003') AS count_jogador_mundial_rank_1 FROM DUAL;
+-- CREATE OR REPLACE FUNCTION select_based_on_input(user_input text) RETURNS TABLE(column_name data_type) AS $$
+-- BEGIN
+--     RETURN QUERY SELECT column_name FROM your_table WHERE some_condition = user_input;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- CREATE OR REPLACE FUNCTION ClassificacoesCampeonato (input_camp text) RETURNS TABLE AS $$
+-- BEGIN
+--     RETURN QUERY (SELECT Equipe.nome, Campeonatos_Participados_Equipe.ranking 
+--     FROM Equipe INNER JOIN Campeonatos_Participados_Equipe
+--     WHERE Campeonatos_Participados_Equipe = input_camp AND
+--     WHERE Equipe.id_equipe = Campeonatos_Participados_Equipe
+--     )
+    
+-- END
+
+SELECT 
+    Campeonato.nome, Equipe.nome, Campeonatos_Participados_Equipe.ranking FROM ((Equipe 
+    INNER JOIN Campeonatos_Participados_Equipe ON Equipe.id_equipe = Campeonatos_Participados_Equipe.id_equipe)
+    INNER JOIN Campeonato ON Campeonato.id_campeonato = Campeonatos_Participados_Equipe.id_campeonato
+    )
+
+
+
+-- ClassificacoesCampeonato('C#001');
+
+-- CREATE OR REPLACE FUNCTION ContarJogadorRank1Mundial(
+--     p_id_jogador IN Jogador.id_jogador%TYPE
+-- )
+
+-- RETURN NUMBER
+-- IS
+--     v_count NUMBER := 0;
+-- BEGIN
+--     SELECT COUNT(*) INTO v_count
+--     FROM Jogador j
+--     JOIN Jogador_Em_Equipe_Data jed ON j.id_jogador = jed.id_jogador
+--     JOIN Equipe e ON jed.equipe_na_data = e.id_equipe
+--     JOIN Campeonatos_Participados_Equipe cpe ON e.id_equipe = cpe.id_equipe
+--     JOIN Campeonato c ON cpe.id_campeonato = c.id_campeonato
+--     WHERE j.id_jogador = p_id_jogador
+--     AND c.tipo = 'Mundial'
+--     AND cpe.ranking = 1;
+
+--     RETURN v_count;
+-- EXCEPTION
+--     WHEN NO_DATA_FOUND THEN
+--         RETURN 0;
+--     WHEN OTHERS THEN
+--         -- Handle exceptions here if needed
+--         RETURN -1;
+-- END;
+-- /
+
+-- SELECT ContarJogadorRank1Mundial('J#003') AS count_jogador_mundial_rank_1 FROM DUAL;
