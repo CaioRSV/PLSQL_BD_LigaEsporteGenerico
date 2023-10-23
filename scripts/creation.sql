3268bd04c1f402f4015723f411f4f4f7107db3b4
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-10-23 07:37:25 -0300

    Update creation.sql

--- scripts/creation.sql
+++ scripts/creation.sql
@@ -1,70 +1,57 @@
-CREATE TABLE Equipe (
-    id_equipe VARCHAR2(50),
-    nome VARCHAR2(50),
-    data_criacao TIMESTAMP NOT NULL,
-    campeonatos NUMBER,
-    trofeus NUMBER,
-
-    CONSTRAINT equipe_pk PRIMARY KEY (id_equipe)
-    
-    );
-
 CREATE TABLE Residencia(
-    cod_residencia_unificado VARCHAR2(50),
-    pais_atual VARCHAR2(50),
+    residencia_cod VARCHAR2(50),
+    pais VARCHAR2(50),
     estado VARCHAR2(50),
     municipio VARCHAR2(50),
 
-    CONSTRAINT residencia_pk PRIMARY KEY (cod_residencia_unificado)
-    
-    );
-
-CREATE TABLE Jogador(
-    id_jogador VARCHAR2(50),
-	nome VARCHAR2(50),
-    data_nascimento TIMESTAMP NOT NULL,
-    nacionalidade VARCHAR2(50),
-    cod_residencia_unificado VARCHAR2(50),
-    equipe_atual VARCHAR2(50),
-    campeonatos NUMBER,
-    trofeus NUMBER,
-
-    CONSTRAINT jogador_pk PRIMARY KEY (id_jogador),
-    CONSTRAINT cod_residencia_unificado_fk FOREIGN KEY (cod_residencia_unificado) REFERENCES Residencia(cod_residencia_unificado),
-    CONSTRAINT equipe_atual_fk FOREIGN KEY (equipe_atual) REFERENCES Equipe(id_equipe)
-    
-    );
-
-CREATE TABLE Jogador_Em_Equipe_Data(
-    id_jogador VARCHAR2(50),
-    ano TIMESTAMP NOT NULL,
-    equipe_na_data VARCHAR2(50),
-
-    CONSTRAINT jogadorEquipeData_pk PRIMARY KEY (id_jogador, ano),
-    CONSTRAINT jogadorEquipeData_jogador_fk FOREIGN KEY (id_jogador) REFERENCES Jogador(id_jogador),
-    CONSTRAINT jogadorEquipeData_equipe_fk FOREIGN KEY (equipe_na_data) REFERENCES Equipe(id_equipe)
+    CONSTRAINT Residencia_pk PRIMARY KEY (residencia_cod)
 );
-
-
-CREATE TABLE Campeonato(
-    id_campeonato VARCHAR2(50),
+--
+CREATE TABLE Pessoa(
+    id_code VARCHAR2(50),
     nome VARCHAR2(50),
-    ano TIMESTAMP NOT NULL,
-    tipo VARCHAR2(50),
-    equipe_campea VARCHAR2(50),
-    
-    CONSTRAINT campeonato_pk PRIMARY KEY (id_campeonato),
-    CONSTRAINT equipe_campea_fk FOREIGN KEY (equipe_campea) REFERENCES Equipe(id_equipe)
+    email VARCHAR2(50),
+    cod_id_nacional VARCHAR2(50),
+    data_nascimento DATE,
+    status NUMBER, -- 1: Em atividade , 2: Em pausa, 3: Aposentado
+    residencia_cod VARCHAR2(50),
 
+    CONSTRAINT Pessoa_pk PRIMARY KEY (id_code),
+    CONSTRAINT Residencia_fk FOREIGN KEY (residencia_cod) REFERENCES Residencia (residencia_cod)
 );
+--
+CREATE TABLE Telefone(
+    telefone NUMBER,
+    pessoa VARCHAR(50),
 
-CREATE TABLE Campeonatos_Participados_Equipe(
+    CONSTRAINT Telefone_fk FOREIGN KEY (pessoa) REFERENCES Pessoa (id_code)
+);
+--
+CREATE TABLE Equipe(
     id_equipe VARCHAR2(50),
-    id_campeonato VARCHAR2(50),
-    ranking NUMBER,
+    nome VARCHAR2(50),
+    data_criacao DATE,
 
-    CONSTRAINT campPart_pk PRIMARY KEY (id_equipe, id_campeonato),
-    CONSTRAINT campPart_equipe_fk FOREIGN KEY (id_equipe) REFERENCES Equipe(id_equipe),
-    CONSTRAINT campPart_camp_fk FOREIGN KEY (id_campeonato) REFERENCES Campeonato(id_campeonato)
+    CONSTRAINT Equipe_pk PRIMARY KEY (id_equipe)
 );
+--
+CREATE TABLE Jogador(
+    id_code VARCHAR2(50),
+    posicao_oficio VARCHAR2(50),
+    num_camisa NUMBER,
+    apelido VARCHAR2(50),
+    time_atual VARCHAR2(50),
 
+    CONSTRAINT Jogador_pessoa_fk FOREIGN KEY (id_code) REFERENCES Pessoa (id_code),
+    CONSTRAINT Jogador_equipe_fk FOREIGN KEY (time_atual) REFERENCES Equipe (id_equipe)
+);
+--
+CREATE TABLE Tecnico(
+    id_code VARCHAR2(50),
+    estilo_tatico VARCHAR2(50),
+    equipe_atual VARCHAR2(50),
+
+    CONSTRAINT Tecnico_pessoa_fk FOREIGN KEY (id_code) REFERENCES Pessoa (id_code),
+    CONSTRAINT Tecnico_equipe_fk FOREIGN KEY (equipe_atual) REFERENCES Equipe (id_equipe)
+);
+--
