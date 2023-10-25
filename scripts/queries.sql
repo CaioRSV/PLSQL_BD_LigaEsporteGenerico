6cdf27814e6cd320d3b0260138964da1f4af951e
Caio Verçosa <31866855+CaioRSV@users.noreply.github.com>
2023-10-25 14:13:38 -0300

    Update queries.sql

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -7,3 +7,27 @@ select * from Telefone;
 select * from Equipe;
 
 select * from Jogador;
+
+SELECT * FROM Tecnico;
+
+SELECT * FROM Pontuacao_anual;
+
+SELECT * FROM Campeonato;
+
+SELECT * FROM Grupo_midiatico;
+
+SELECT * FROM Jogador_participa_camp;
+
+SELECT * FROM Equipe_participa_camp;
+
+SELECT * FROM Pontua;
+
+SELECT * FROM Declaracao;
+
+SELECT * FROM Dec_Ref_Pessoa;
+
+SELECT * FROM Dec_Ref_Equipe;
+
+SELECT * FROM Dec_Ref_Campeonato;
+
+SELECT * FROM Dec_Ref_GM;
